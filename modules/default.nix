11f64938a95256a9bb0a4f6ed7e9bfa54df1629b
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2023-01-25 10:19:24 -0700

    Add additional defaults
    Adds most of the defaults based on this blog post: https://jackson.dev/post/nix-reasonable-defaults/)

--- modules/default.nix
+++ modules/default.nix
@@ -10,4 +10,15 @@
   nix.settings.trusted-public-keys = [
     "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
   ];
+  
+  # Reasonable Defaults
+  connect-timeout = 5
+  log-lines = 25
+  min-free = 128000000
+  max-free = 1000000000
+
+  experimental-features = nix-command flakes
+  fallback = true
+  warn-dirty = false
+  auto-optimise-store = true
 }
