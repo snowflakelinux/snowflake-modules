96a668e3f97a129c7c55cb440d766f8b34c0254d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-02-12 20:04:51 +0100

    Use lib.mkDefault for none mergeable settings

--- modules/snowflake/default.nix
+++ modules/snowflake/default.nix
@@ -6,24 +6,24 @@
     ./hardware.nix
     ./version.nix
   ];
-  nix = {
-    settings = {
-      substituters = [ "https://snowflakeos.cachix.org/" ];
-      trusted-public-keys = [
-        "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
-      ];
-      # Reasonable Defaults
-      connect-timeout = 5;
-      log-lines = 25;
-      min-free = 128000000;
-      max-free = 1000000000;
-      experimental-features = [ "nix-command" "flakes" ];
-      fallback = true;
-      warn-dirty = false;
-      auto-optimise-store = true;
-    };
+
+  # Reasonable Defaults
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [ "https://snowflakeos.cachix.org/" ];
+    trusted-public-keys = [
+      "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
+    ];
+  } // (lib.mapAttrsRecursive (_: lib.mkDefault) {
+    connect-timeout = 5;
+    log-lines = 25;
+    min-free = 128000000;
+    max-free = 1000000000;
+    fallback = true;
+    warn-dirty = false;
+    auto-optimise-store = true;
     linkInputs = true;
     generateNixPathFromInputs = true;
     generateRegistryFromInputs = true;
-  };
+  });
 }
