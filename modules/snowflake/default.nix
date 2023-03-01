a7a0cd41a6c1c1eaa06a31fbb5f0f0d3d6ba56ca
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-03-01 16:35:39 +0100

    Move flake-utils-plus options to the correct place

--- modules/snowflake/default.nix
+++ modules/snowflake/default.nix
@@ -8,20 +8,23 @@
   ];
 
   # Reasonable Defaults
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    substituters = [ "https://snowflakeos.cachix.org/" ];
-    trusted-public-keys = [
-      "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
-    ];
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://snowflakeos.cachix.org/" ];
+      trusted-public-keys = [
+        "snowflakeos.cachix.org-1:gXb32BL86r9bw1kBiw9AJuIkqN49xBvPd1ZW8YlqO70="
+      ];
+    } // (lib.mapAttrsRecursive (_: lib.mkDefault) {
+      connect-timeout = 5;
+      log-lines = 25;
+      min-free = 128000000;
+      max-free = 1000000000;
+      fallback = true;
+      warn-dirty = false;
+      auto-optimise-store = true;
+    });
   } // (lib.mapAttrsRecursive (_: lib.mkDefault) {
-    connect-timeout = 5;
-    log-lines = 25;
-    min-free = 128000000;
-    max-free = 1000000000;
-    fallback = true;
-    warn-dirty = false;
-    auto-optimise-store = true;
     linkInputs = true;
     generateNixPathFromInputs = true;
     generateRegistryFromInputs = true;
