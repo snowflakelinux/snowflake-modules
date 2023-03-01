db207ccd28c35514c2b6f15bfef1c1eb00a11d66
Victor Fuentes <vmfuentes64@gmail.com>
2023-03-01 11:44:13 -0500

    Update nix-software-center and snowfall lib

--- packages/nix-software-center/default.nix
+++ packages/nix-software-center/default.nix
@@ -6,7 +6,7 @@
   (pkgs.fetchFromGitHub {
     owner = "vlinkz";
     repo = "nix-software-center";
-    rev = "0.1.1";
-    hash = "sha256-98WJom0WtCymJboUom7Uuo8f53JfITR9uqD+L7BifbY=";
+    rev = "64ef8b1bb08fe27863743ea5f135391c7fd287a3";
+    hash = "sha256-RbMm3jnRHA+U35hcNePcp2BsmpCAeuwqjsw7vxhu+o0=";
   })
 { inherit pkgs lib; })
